7ea578b8be1561d54dbe37a94ebb6d866ad1da0d
Meidozuki <68063235+Meidozuki@users.noreply.github.com>
2023-04-16 19:00:03 +0800

    文件框架雏形

---
+++ myMVVM/__init__.py
@@ -0,0 +1,20 @@
+import os, sys
+
+sys.path.append(r"D:\coding\git\VBAO\Lib_VBao\python")
+
+from vbao import Model, ViewModel, View, App
+
+
+class GameMainLogic(Model):
+    """
+    这是用来实现游戏业务逻辑的类
+
+    """
+    pass
+
+class GameViewModel(ViewModel):
+    pass
+
+
+class UserInterface(View):
+    pass
--- run_game.py
+++ run_game.py
@@ -6,22 +6,32 @@ class Window:
     def __init__(self):
         pass
 
-    def start(self):
+    def start(self, title="Hello World"):
         pygame.init()
+        pygame.display.set_caption(title)
         screen = pygame.display.set_mode(size=(800, 600))
+
+        pic = pygame.image.load("local/img/craftpix-net-725990-octopus-jellyfish-shark-and-turtle-free-sprite-pixel-art/2/Idle.png")
+        screen.blit(pic,(100,100))
 
 class GameApp(App):
     def __init__(self):
-        pass
+        self.window = Window()
 
     def run(self):
 
         try:
             self.window.start()
+
+            some_flag=1
             while some_flag:
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
-                        raise KeyboardInterrupt()
+                        raise KeyboardInterrupt("Game window closed by user.")
+
+                    pygame.display.update()
+        except KeyboardInterrupt as e:
+            print(e.args)
         finally:
             pygame.quit()
 
