21bd762fb5bfb3e7e38d63d1807125346ee1655f
Meidozuki <68063235+Meidozuki@users.noreply.github.com>
2023-04-18 16:22:00 +0800

    最简单的游戏逻辑，同时修复一行全部为dead zone的情况

--- myMVVM/GameLogic.py
+++ myMVVM/GameLogic.py
@@ -7,27 +7,61 @@ from .common import ConstValue
 class GameMainLogic(vbao.Model):
     """
     这是用来实现游戏业务逻辑的类
-
     """
 
     def __init__(self):
         super().__init__()
 
-        self.possible_grid_types = (0,3)
+        self.possible_grid_types = (0,2)
 
-    def ctx(self):
-        return ConstValue(self.board)
+
+    # Game logic
+    def generate(self, shape):
+        x = np.random.randint(*self.possible_grid_types, shape)
+        b = np.all(x != 0, axis=1)
+        while np.any(b):
+            idx = np.nonzero(b)[0]
+            x[idx] = np.random.randint(*self.possible_grid_types, (idx.shape[0], shape[1]))
+            b = np.all(x != 0, axis=1)
+        return x
+
 
     def gameInit(self):
+        self.combo = 0
+        self.score = 0
         self.row = self.property["row"].x
         self.col = self.property["col"].x
-        self.board = np.random.randint(*self.possible_grid_types, [self.row, self.col])
+        self.board = self.generate([self.row, self.col])
 
+    def stepOnGrid(self, grid_no):
+        if self.board[0, grid_no] == 0:
+            self.combo += 1
+        else:
+            self.combo = 0
+        self.updateScore()
 
-    def gameOver(self):
+        self.board[:-1] = self.board[1:]
+        self.board[-1] = self.generate([1, self.col])
 
-        return self.calScore()
+        self.calScore()
+
+    def updateScore(self, pure=False):
+        if pure:
+            return self.score
+
+        self.score += self.combo*5
+
 
     def calScore(self):
-        self.property["score"] = f()
+        self.property["score"] = self.updateScore(pure=True)
         self.triggerPropertyNotifications("score")
+
+    def printScore(self):
+        print(f"score = {self.score}, combo = {self.combo}")
+
+    def gameOver(self):
+        return self.calScore()
+
+    # For data exchanging
+    def ctx(self):
+        return ConstValue(self.board)
